17ee21d79d45c1a1e647f0ff10038b92dab4bb3a
Liz Burton <liz.burton147@gmail.com>
2020-10-30 12:19:50 -0400

    Updates solution code to work in the browser

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import SimplerComponent from './components/SimplerComponent';
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={alert("clicked!")} />
+    <SimplerComponent handleClick={() => alert("clicked!")} />
   </div>,
   document.getElementById('root')
 );
